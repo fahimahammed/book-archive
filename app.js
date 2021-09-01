19ace01dcdae1c97466fd82726d81d3f8f31b514
fahimahammed <fahimahammed.cse@gmail.com>
2021-09-02 01:29:48 +0600

    book archive

---
+++ app.js
@@ -0,0 +1,55 @@
+const searchBook = () => {
+    const searchText = document.getElementById('search-field').value;
+    const url = `http://openlibrary.org/search.json?q=${searchText}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => randerData(data.docs));
+}
+
+
+
+const randerData = (data) => {
+    
+    const booksContainer = document.getElementById('books-container');
+    booksContainer.className = 'row';
+    
+    document.getElementById("result-num").innerHTML = `<h6>${data.length} num</h6>`;
+
+    booksContainer.innerHTML = '';
+
+    data.forEach(element => {
+
+        let publishYear = "";
+        let bookPublisher = "";
+        const authors = element.author_name;
+        
+        element.publish_year ? publishYear = element.publish_year[0] : "N/A";
+        element.publisher ? bookPublisher = element.publisher[0] : "N/A";
+        
+
+        element.cover_i && console.log(element.cover_i);
+
+        const bookDiv = document.createElement('div');
+        const authorDiv = document.createElement('ul');
+        bookDiv.className = "col-md-4";
+
+        bookDiv.innerHTML = `
+                <img src = "https://covers.openlibrary.org/b/id/${element.cover_i}-M.jpg"/>
+                <h5>${element.title}</h5>
+                <p>Publish Year: ${publishYear}</p>
+                <p>Publisher: ${bookPublisher}</p>
+        `;
+
+        authors ? authors.forEach(author => {
+            authorDiv.innerHTML = `
+                <li>${author}</li>
+            `
+        }) : "";
+        bookDiv.appendChild(authorDiv);
+        booksContainer.appendChild(bookDiv);
+        document.getElementById('search-field').value = "";
+        
+        
+    });
+    
+}
